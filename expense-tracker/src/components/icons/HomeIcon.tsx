77badcc0249b5590ecb41d62af32d45998f81ca8
Boris <borisnais222@gmail.com>
2026-09-10 13:11:20 +0300

    refactor: make Planer native project the repository root

--- expense-tracker/src/components/icons/HomeIcon.tsx
+++
@@ -1,9 +0,0 @@
-export const HomeIcon = () => (
-  <svg width="26" height="26" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M12 2.1L1 12h3v9a1 1 0 001 1h5v-6h4v6h5a1 1 0 001-1v-9h3L12 2.1z"
-      fill="currentColor"
-      opacity="0.9"
-    />
-  </svg>
-);
--- expense-tracker/src/types/build.d.ts
+++
@@ -1,1 +0,0 @@
-declare const __APP_BUILD_VERSION__: number;
